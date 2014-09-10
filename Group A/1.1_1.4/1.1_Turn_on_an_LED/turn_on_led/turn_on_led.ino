df56ad3a77a56c9fe8f113165d0336650da4a9ca
Derek Wider <technoD@outlook.com>
2014-09-09 22:16:43 -0400

    Finished Programs 1.1 - 2.2
    Programs 1.1 - 2.2 (6 total) are finished and working!
    See TODO.txt for remaining programs to write/fix with new pinouts

--- Group A/1.1_1.4/1.1_Turn_on_an_LED/turn_on_led/turn_on_led.ino
+++ Group A/1.1_1.4/1.1_Turn_on_an_LED/turn_on_led/turn_on_led.ino
@@ -1,11 +1,11 @@
 /* Software 1073 Example Program
 Written and debugged for use on the Arduino boebot platform
-last updated: 8/14/14
+last updated: 9/9/14
 
-Program: Turns on an LED on pin 9
+Program: Turns on an LED on pin 5
 */
 
-int led = 9;  //creates an int called "led" with a value of 9
+int led = 5;  //creates an int called "led" with a value of 9
 
 void setup(){ //setup runs just once
   pinMode(led, OUTPUT); //establishes the led as an output
