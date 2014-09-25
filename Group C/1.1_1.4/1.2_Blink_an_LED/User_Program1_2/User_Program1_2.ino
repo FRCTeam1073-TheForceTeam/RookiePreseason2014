f0caccc21a017ce8deaf2f18ead85f2b943e00a8
Anish <evinoog96@gmail.com>
2014-09-25 17:17:11 -0400

    Revert "Group C's Work, 9/24/14"
    This reverts commit 0aa3b786baab718eb3cb9a8ea03bb2f9740bed91.

--- Group C/1.1_1.4/1.2_Blink_an_LED/User_Program1_2/User_Program1_2.ino
+++ Group C/1.1_1.4/1.2_Blink_an_LED/User_Program1_2/User_Program1_2.ino
@@ -5,42 +5,12 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Blink an LED on pin 5 
 Feel free to view the example program within the same folder if you need help!
 */
-int led = 5;
 
 void setup(){
-  pinMode(led, OUTPUT);
+  //your code goes here
 }
 
-boolean on = false;
-int count = 0;
-
-/*
-
-void loop()
-{
-  if (count == 20000)
-  {
-    if (on == true)
-    {
-      digitalWrite(led, LOW);
-      on = false;
-    }
-    else
-    {
-      digitalWrite(led, HIGH);
-      on = true;
-    }
-    count = 0;
-  }
-  count = count + 1;
-}
-*/
-
-void loop()
-{
-  digitalWrite(led, LOW);
-  delay(500);
-  digitalWrite(led, HIGH);
-  delay(500);
+void loop(){
+  //also here
 }
 
--- Group C/1.1_1.4/1.3_Cycle_LEDs/User_Program1_3/User_Program1_3.ino
+++ Group C/1.1_1.4/1.3_Cycle_LEDs/User_Program1_3/User_Program1_3.ino
@@ -5,18 +5,12 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Cycle the 3 leds of the robot, so that one led turns on, then the next, then the next
 Feel free to view the example program within the same folder if you need help!
 */
-int redled = 5;
-int yellowled = 3;
-int greenled = 6;
+
 void setup(){
-  pinMode(redled, OUTPUT);
-  pinMode(yellowled, OUTPUT);
-  pinMode(greenled, OUTPUT);
+  //your code goes here
 }
 
 void loop(){
-  digitalWrite(redled, HIGH);
-  digitalWrite(yellowled, HIGH);
-  digitalWrite(greenled, HIGH);
+  //also here
 }
 
