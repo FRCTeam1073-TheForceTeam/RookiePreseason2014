926afe6fdeac8981f86c4ba631c78814ca9479b1
Derek Wider <technoD@outlook.com>
2014-09-25 17:15:19 -0400

    Group B's Work, 9/24/14

--- Group B/1.1_1.4/1.3_Cycle_LEDs/User_Program1_3/User_Program1_3.ino
+++ Group B/1.1_1.4/1.3_Cycle_LEDs/User_Program1_3/User_Program1_3.ino
@@ -5,12 +5,30 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Cycle the 3 leds of the robot, so that one led turns on, then the next, then the next
 Feel free to view the example program within the same folder if you need help!
 */
+int led = 5;
+int led2 = 3;
+int led3 = 6;
 
 void setup(){
-  //your code goes here
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  
 }
 
 void loop(){
-  //also here
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500);s
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+  delay(500);
+  digitalWrite(led3, HIGH);
+  delay(500);
+  digitalWrite(led3, LOW);
+  delay(500);
+  
 }
 
--- Group B/1.1_1.4/1.4_Dim_an_LED_for_loop/User_Program1_4/User_Program1_4.ino
+++ Group B/1.1_1.4/1.4_Dim_an_LED_for_loop/User_Program1_4/User_Program1_4.ino
@@ -5,12 +5,40 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Fade in and out the led on pin 5 using 2 for loops
 Feel free to view the example program within the same folder if you need help!
 */
-
+int led=5;
+int led2=3;
+int led3=6;
+int blinked=20;
 void setup(){
-  //your code goes here
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  
 }
 
 void loop(){
-  //also here
+  for(int i=0; i < 255; i=i + 5){
+    analogWrite(led, i);
+    delay(blinked);
+  }
+  for(int i=255; i >= 0;i -= 5){
+    analogWrite(led, i);
+    delay(blinked);
+  }
+   for(int i=0; i < 255; i=i + 5){
+    analogWrite(led2, i);
+    delay(blinked);
+  }
+  for(int i=255; i >= 0;i -= 5){
+    analogWrite(led2, i);
+    delay(blinked);
+  }
+   for(int i=0; i < 255; i=i + 5){
+    analogWrite(led3, i);
+    delay(blinked);
+  }
+  for(int i=255; i >= 0;i -= 5){
+    analogWrite(led3, i);
+    delay(blinked);
+  }
 }
-
