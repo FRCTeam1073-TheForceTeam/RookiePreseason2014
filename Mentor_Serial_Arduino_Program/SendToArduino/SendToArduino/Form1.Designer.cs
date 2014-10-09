bafefbe310acdd97af598535df29b970ceb80850
Derek Wider <technoD@outlook.com>
2014-10-08 21:13:04 -0700

    Mr.Cowan's C# serial coms application
    work in progress

---
+++ Mentor_Serial_Arduino_Program/SendToArduino/SendToArduino/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace SendToArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendButton = new System.Windows.Forms.Button();
+            this.valueBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fromArduinoBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(93, 88);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 0;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // valueBox
+            // 
+            this.valueBox.Location = new System.Drawing.Point(93, 52);
+            this.valueBox.Name = "valueBox";
+            this.valueBox.Size = new System.Drawing.Size(100, 20);
+            this.valueBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "To Arduino";
+            // 
+            // fromArduinoBox
+            // 
+            this.fromArduinoBox.Location = new System.Drawing.Point(93, 145);
+            this.fromArduinoBox.Name = "fromArduinoBox";
+            this.fromArduinoBox.Size = new System.Drawing.Size(100, 20);
+            this.fromArduinoBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "From Arduino";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fromArduinoBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.valueBox);
+            this.Controls.Add(this.sendButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox valueBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fromArduinoBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Mentor_Serial_Arduino_Program/SendToArduino/SendToArduino/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace SendToArduino
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string portName = SendToArduino.Properties.Settings.Default.PortName;
+            port = new SerialPort(portName);
+            port.Open();
+            port.DataReceived += port_DataReceived;
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            string dataToSend = valueBox.Text + "\n";
+            var asciiData = Encoding.ASCII.GetBytes(dataToSend);
+
+            port.Write(asciiData, 0, asciiData.Length);
+        }
+
+        private void port_DataReceived(object sender, EventArgs e)
+        {
+            Console.WriteLine("Received data from Arduino");
+            var dataRead = port.ReadLine();
+            fromArduinoBox.Text = dataRead;
+            
+        }
+    }
+}
