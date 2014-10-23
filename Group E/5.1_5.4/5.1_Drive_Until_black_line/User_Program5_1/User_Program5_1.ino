81f41daa994838108b3951cf16ed98fee559809e
Evin Ugur <evinoog96@gmail.com>
2014-10-22 20:17:27 -0400

    Group E's Work 10/22/14 Finished up to 5.1
    Some intense coding happened here today

--- Group E/5.1_5.4/5.1_Drive_Until_black_line/User_Program5_1/User_Program5_1.ino
+++ Group E/5.1_5.4/5.1_Drive_Until_black_line/User_Program5_1/User_Program5_1.ino
@@ -6,11 +6,36 @@ Program Task: Have the robot drive forward until it sees a black line
 Feel free to view the example program within the same folder if you need help!
 */
 
+#include <Servo.h>;
+Servo left;
+Servo right;
+
+int leftLight = 0;
+int rightLight = 1;
+int leftLightVal = 0;
+int rightLightVal = 0;
+int leftWhiteVal = 390;
+int rightWhiteVal = 450;
+int changeVal = 150;
+
+void readSensors(){
+  leftLightVal = analogRead (leftLight);
+  rightLightVal = analogRead (rightLight);
+}
 void setup(){
-  //your code goes here
+  left.attach(9);
+  right.attach(10);
 }
 
 void loop(){
-  //also here
+  readSensors();
+  if (leftLightVal >= leftWhiteVal + changeVal || rightLightVal >= rightWhiteVal + changeVal){
+   right.writeMicroseconds (1500);
+  left.writeMicroseconds (1500); 
+  }
+  else {
+  right.writeMicroseconds (1700);
+  left.writeMicroseconds (1300);
+  }
 }
 
