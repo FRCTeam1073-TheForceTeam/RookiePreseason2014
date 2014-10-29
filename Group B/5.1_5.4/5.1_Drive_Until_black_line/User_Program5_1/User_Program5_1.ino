b9c1862387b348f40feb65cf153593ca583d8ba8
Derek Wider <technoD@outlook.com>
2014-10-29 19:06:59 -0400

    Group C's Work, 10/22/14

--- Group B/5.1_5.4/5.1_Drive_Until_black_line/User_Program5_1/User_Program5_1.ino
+++ Group B/5.1_5.4/5.1_Drive_Until_black_line/User_Program5_1/User_Program5_1.ino
@@ -5,12 +5,49 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Have the robot drive forward until it sees a black line
 Feel free to view the example program within the same folder if you need help!
 */
+#include <Servo.h>
 
+Servo left;
+Servo right;
+int forward = 1700;
+int forwar = 1300;
+int poop = 1500;
+int sensright = 1;
+int sensleft = 0;
+int leftnum = 0;
+int rightnum = 0;
+
+void readsensor(){
+  rightnum = analogRead(sensright);
+  leftnum = analogRead(sensleft);
+  Serial.println(" Left ");
+  Serial.print(leftnum);
+  delay(500);
+  Serial.println("");
+  Serial.println(" Right ");
+  Serial.print(rightnum);
+  Serial.println("");
+  delay(500);
+}
+
+void godrivego(){
+  left.writeMicroseconds(forward);
+  right.writeMicroseconds(forwar);
+  
+}
 void setup(){
-  //your code goes here
+  Serial.begin(9600);
+  left.attach(10);
+  right.attach(9);
 }
 
 void loop(){
-  //also here
+  readsensor();
+  godrivego();
+  if(leftnum + rightnum >= 1900){
+      left.writeMicroseconds(poop);
+      right.writeMicroseconds(poop);
+  }
+  
 }
 
