bb5c37fcecc3696f8d7a1e5efc12389509b6d77e
Derek Wider <technoD@outlook.com>
2014-10-28 23:29:12 -0400

    added final challenge 1
    no example program. good luck!

---
+++ Group C/final_Challenge_1/final_challenge_1/final_challenge_1.ino
@@ -0,0 +1,21 @@
+/*Software 1073 Arduino BoeBots
+User Program 
+Method stubs are pre-made. add your own code to complete the challenge!
+
+Program Task: Create an autonomous program that has the robot searching for a clear path of travel.
+Logic example (copied from powerpoint, 10/28/14):
+Robot drives forward until it sees a wall
+Checks to the left -> sees another wall
+Checks to the right -> sees a clear pathway. 
+Continues to the right and then repeats the process.
+
+Feel free to not view the example program within the same folder, because it doesn't exist!
+*/
+
+void setup(){
+  //add your code here
+}
+
+void loop(){
+  //and here
+}
