bd5cf1e1954df3ba9c7e0718c9f15d480e39155c
Derek Wider <technoD@outlook.com>
2014-10-29 20:20:49 -0400

    Group C work- 10/29/14
    got through 4.1 and 4.2 (issues with led cycle)
    
    -Mitch

--- Group C/4.1_4.2/4.1_Drive_Robot_Until_ultra/User_Program4_1/User_Program4_1.ino
+++ Group C/4.1_4.2/4.1_Drive_Robot_Until_ultra/User_Program4_1/User_Program4_1.ino
@@ -7,18 +7,42 @@ Feel free to view the example program within the same folder if you need help!
 */
 
 #include <Servo.h>
-#include <NewPing.h>
-int output = 12;
-int input = 8;
-int MAX = 200;
+#include<NewPing.h>
+
+
+int Trigger_Pin = 12;
+int Echo_Pin = 8; 
+int max_distance = 200; 
+NewPing sonar(Trigger_Pin, Echo_Pin, max_distance);
+
+Servo left;
+Servo right; 
+
+int getDistance() {
+  delay(50);
+  int ping = sonar.ping();
+  int centd = ping/US_ROUNDTRIP_CM;
+  return centd;
+}
 
 
 
 void setup(){
-  //your code goes here
+left.attach(10);
+right.attach(9);
+
 }
 
 void loop(){
-  //also here
+if(getDistance() > 10)
+  drive();
+else  { 
+  left.writeMicroseconds(1500);
+  right.writeMicroseconds(1500);
+}
 }
 
+void drive(){
+left.writeMicroseconds(1700);
+right.writeMicroseconds(1300);
+}
--- Group C/4.1_4.2/4.2_Drive_Robot_Until_ultra_with_LED/User_Program4_2/User_Program4_2.ino
+++ Group C/4.1_4.2/4.2_Drive_Robot_Until_ultra_with_LED/User_Program4_2/User_Program4_2.ino
@@ -6,11 +6,71 @@ Program Task: Have the robot cycle the leds while driving, but if it sees a wall
 Feel free to view the example program within the same folder if you need help!
 */
 
+#include <Servo.h>
+#include<NewPing.h>
+
+
+int Trigger_Pin = 12;
+int Echo_Pin = 8; 
+int max_distance = 200; 
+NewPing sonar(Trigger_Pin, Echo_Pin, max_distance);
+int Buzzer = 11;
+int redled = 5;
+int yellowled = 3;
+int greenled = 6;
+int counter = 0;
+
+
+
+
+Servo left;
+Servo right; 
+
+int getDistance() {
+  delay(50);
+  int ping = sonar.ping();
+  int centd = ping/US_ROUNDTRIP_CM;
+  return centd;
+}
+
+
+
 void setup(){
-  //your code goes here
+left.attach(10);
+right.attach(9);
+pinMode(Buzzer, OUTPUT);
+pinMode(redled, OUTPUT);
+pinMode(greenled, OUTPUT);
+pinMode(yellowled, OUTPUT);
 }
 
 void loop(){
-  //also here
+counter += 1;
+if(counter < 1000) {
+digitalWrite(redled, LOW);
+digitalWrite(yellowled, HIGH);
+}
+else if(counter < 2000) {
+digitalWrite(yellowled, LOW);
+digitalWrite(greenled, HIGH);
+}
+else if(counter < 3000) {
+digitalWrite(greenled, LOW);
+digitalWrite(redled, HIGH);
+}
+else  {
+counter = 0;
 }
 
+if(getDistance() > 10)
+  drive();
+else  { 
+  left.writeMicroseconds(1500);
+  right.writeMicroseconds(1500);
+}
+}
+
+void drive(){
+left.writeMicroseconds(1700);
+right.writeMicroseconds(1300);
+}
